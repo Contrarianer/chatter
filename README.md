f7d97e41998af5e359b23abb64a4f49fc2f486d0
Payne <wuzhipeng1289690157@gmail.com>
2022-01-12 15:58:59 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## chatter
+
+a simple web example 
+
+## requirement
+
+```text
+express 
+multer 
+nodemon 
+body-parser 
+cookie-parser
+
+
+// ES6 => ES5
+babel
+babel-cli
+babel-preset-env
+babel-register
+babel-preset-es2015
+babel-preset-stage-2
+```
---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const log4js = require('log4js');
+
+// load config
+log4js.configure("./conf/log4js.json")
+const logger = log4js.getLogger("dev");
+
+// serve config
+const app = express(),
+    host = process.env.HOST || '0.0.0.0',
+    port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    let res_data = {"a": 1};
+    // res.send(JSON.stringify(res_data));
+    res.json(res_data);
+});
+
+app.listen(port, () => {
+    logger.info('Listening on %d ...:', port);
+    logger.info('Please Visit Index:   http://%s:%d ...:', host, port);
+})
