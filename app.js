f6821fc60686e74807cdefd15a786fe5f474e48e
Payne <wuzhipeng1289690157@gmail.com>
2022-01-12 16:23:22 +0800

    add test api

--- app.js
+++ app.js
@@ -11,11 +11,16 @@ const app = express(),
     port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
+    res.send("<h2 align='center'>Welcome Chatter System</h2>")
+});
+
+app('/json_example', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     let res_data = {"a": 1};
     // res.send(JSON.stringify(res_data));
     res.json(res_data);
-});
+})
+
 
 app.listen(port, () => {
     logger.info('Listening on %d ...:', port);
