059ae3dbfdc40b865bcc6722c1be057f123041cd
Payne <wuzhipeng1289690157@gmail.com>
2022-01-14 17:13:51 +0800

    upadte

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const log4js = require('log4js');
 
 // load config
 log4js.configure("./conf/log4js.json")
-const logger = log4js.getLogger("dev");
+const logger = log4js.getLogger("test");
 
 // serve config
 const app = express(),
@@ -14,9 +14,10 @@ app.get('/', function (req, res) {
     res.send("<h2 align='center'>Welcome Chatter System</h2>")
 });
 
-app('/json_example', (req, res) => {
+app.get('/json_example', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    let res_data = {"a": 1};
+    a = [1, 3, 2]
+    let res_data = { "a": a };
     // res.send(JSON.stringify(res_data));
     res.json(res_data);
 })
@@ -24,5 +25,5 @@ app('/json_example', (req, res) => {
 
 app.listen(port, () => {
     logger.info('Listening on %d ...:', port);
-    logger.info('Please Visit Index:   http://%s:%d ...:', host, port);
+    logger.debug('Please Visit Index:   http://%s:%d ...:', host, port);
 })
