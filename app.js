324b3946c2d60c940165c5c9925f5ac8144dd9c2
Payne <wuzhipeng1289690157@gmail.com>
2022-01-17 12:55:07 +0800

    upadte

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const express = require('express');
-const log4js = require('log4js');
-
-// load config
-log4js.configure("./conf/log4js.json")
-const logger = log4js.getLogger("test");
+const {logger, db} = require("./conf/config");
 
 // serve config
 const app = express(),
@@ -11,19 +7,27 @@ const app = express(),
     port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
+    logger.info(req.hostname + ' - - "GET ' + req.url + ' HTTP/1.1" 200 -')
     res.send("<h2 align='center'>Welcome Chatter System</h2>")
 });
 
-app.get('/json_example', (req, res) => {
+app.get('/example', (req, res) => {
+    logger.info(req.hostname + ' - - "GET ' + req.url + ' HTTP/1.1" 200 -')
     res.setHeader('Content-Type', 'application/json');
-    a = [1, 3, 2]
-    let res_data = { "a": a };
-    // res.send(JSON.stringify(res_data));
+    let a = [1, 3, 2]
+    let res_data = {"a": a};
     res.json(res_data);
 })
 
 
-app.listen(port, () => {
-    logger.info('Listening on %d ...:', port);
+app.get('/get', (req, res) => {
+    logger.info(req.hostname + ' - - "GET ' + req.url + ' HTTP/1.1" 200 -')
+    const result = req.query.result;
+    res.json({"result": result});
+})
+
+
+app.use(express.json()).listen(port, function () {
+    logger.debug('Listening on %d ...:', port);
     logger.debug('Please Visit Index:   http://%s:%d ...:', host, port);
 })
---
+++ conf/config.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql");
+const log4js = require('log4js');
+
+// load config
+log4js.configure("./conf/log4js.json")
+const logger = log4js.getLogger("test");
+
+var pool = mysql.createPool({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "123456",
+    database: "monitor"
+})
+
+
+var query = function (sql, options, callback) {
+    pool.getConnection(function (err, conn) {
+        if (err) {
+            callback(err, null);
+        } else {
+            conn.query(sql, options, function (err, results) {
+                callback(err, results);
+            });
+            conn.release();
+        }
+    });
+};
+
+module.exports = {
+    query: query,
+    logger: logger,
+}
